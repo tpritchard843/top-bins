b7f03e002f7437e3cc0529c10ce7743fba044c86
unexpectedhaki <tpritchard843@gmail.com>
2023-06-05 15:01:09 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// your code here
+
+fetch("")
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
