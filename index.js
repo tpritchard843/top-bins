00d4e82c0b28ea4bcf220cb1e3698ae4f5908e08
unexpectedhaki <tpritchard843@gmail.com>
2023-06-05 21:54:51 -0400

    Added fetch method

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 // your code here
 
-fetch("")
-    .then(res => res.json()) // parse response as JSON
-    .then(data => {
-      console.log(data)
-    })
-    .catch(err => {
-        console.log(`error ${err}`)
-    });
+var myHeaders = new Headers();
+myHeaders.append("x-rapidapi-key", "1458fbec424eedbdabea3056d8e61cd7");
+myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("https://v3.football.api-sports.io/leagues", requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    console.log(result);
+    console.log(result.response);
+    for (let i = 0; i < 10; i++) {
+        console.log(i, result.response[i])
+    }
+  })
+  .catch(error => console.log('error', error));
+
